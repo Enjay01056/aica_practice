528549c478652940ae379df25773c36643d4ee92
Enjay01056 <sahae015@gmail.com>
2026-07-10 11:20:02 +0900

    add file hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+# 새로 추가한 인사말
+print("=" * 40)
+print("Git 실습 두 번째 커밋 진행 중!")
+print("=" * 40)
+languages = ["Python", "JavaScript", "Java"]
+print("배우고 싶은 언어:")
+for lang in languages:
+    print(f" - {lang}") 
+    
